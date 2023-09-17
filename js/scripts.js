f2443573cb61681b0ef9e6284eea77127811f59d
ya-ty <61456449+ya-ty@users.noreply.github.com>
2023-09-17 22:06:32 +0530

    removed `px` from the root margin.

--- js/scripts.js
+++ js/scripts.js
@@ -25,7 +25,7 @@ window.addEventListener('DOMContentLoaded', event => {
     if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#mainNav',
-            rootMargin: '0px 0px -40%',
+            rootMargin: '0 0 -40%',
         });
     };
 
