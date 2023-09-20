a1a7e20fe1548b8f010f4160abcaf1d3c5ff8e49
Rachit Tyagi <rkumar@tnsinc.com>
2023-09-20 20:26:08 +0530

    rachittyagi/ROV-18 Added service class

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { AuthRoutingModule } from './auth-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [AuthRoutingModule],
+  providers: [],
+})
+export class AuthModule {}
