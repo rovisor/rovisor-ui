b5403aca39cbd5bf8bd6499b11e4d35178d8c7c1
yaneshtyagi <yanesh.tyagi@gmail.com>
2023-11-11 15:00:06 +0530

    Implemented aplication header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [{
   path: '', 
-  component: AppComponent,
+  component: DashboardComponent,
 },  
 { 
   path: 'auth', 
