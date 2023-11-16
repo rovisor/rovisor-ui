20bbdc75545987f212d79e601897643a794d07fd
Vedant <vedanttyagi18@gmail.com>
2023-11-17 01:48:09 +0530

    work on forget_password page

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -8,3 +8,30 @@ import { Component } from '@angular/core';
 export class ForgotPasswordComponent {
   title = 'rovisor-ui';
 }
+export class SignInComponent implements OnInit {
+  public loginForm!: FormGroup;
+  private subscription: Subscription = new Subscription();
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const email = this.loginForm.value.email;
+    const password = this.loginForm.value.password;
+
+    this.subscription.add(this.authService.login(email, password).subscribe((response: LoginResponseModel) => {
+      console.log(response);
+    }));
+
+  }
+}
