b5cec3093f6bde8609236c4af8eac4834281f94d
Vedant <vedanttyagi18@gmail.com>
2023-11-28 23:08:58 +0530

    Generate-an-angular-component-named-registration_2

--- src/app/application/registration_2/registration_2.component.ts
+++ src/app/application/registration_2/registration_2.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-registration',
-    templateUrl: './registration 2.component.html',
-    styleUrls: ['./registration 2.component.css']
+    templateUrl: './registation_2.component.html',
+    styleUrls: ['./registration_2.component.css']
   })
 
 export class registration_2 {
