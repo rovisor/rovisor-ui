f270a547ff9e29395f31f71a1ec05b6af9d7f043
Vedant <vedanttyagi18@gmail.com>
2023-11-28 23:00:04 +0530

    Generate-an-angular-component-named-registration_2

--- src/app/application/application.module.ts
+++ src/app/application/application.module.ts
@@ -4,10 +4,12 @@ import { ApplicationComponent } from './application.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedComponentsModule } from '../shared-components/shared-components.module';
 import { registration} from'./registration 2/registration 2.component';
+import { registration_2} from'./registration_2/registration_2.component';
 @NgModule({
   declarations: [
     ApplicationComponent,
     registration
+    registration_2 
   ],
   imports: [
     ApplicationRoutingModule,
---
+++ src/app/application/registration_2/registration_2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-registration',
+    templateUrl: './registration 2.component.html',
+    styleUrls: ['./registration 2.component.css']
+  })
+
+export class registration_2 {
+
+}
