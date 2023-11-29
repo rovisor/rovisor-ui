913adef34364d7e1dcfe6f8f4af0f5e9fa3726f9
Vedant <vedanttyagi18@gmail.com>
2023-11-29 20:27:14 +0530

    made the changes in aaplication module

--- src/app/application/application.module.ts
+++ src/app/application/application.module.ts
@@ -3,7 +3,6 @@ import { ApplicationRoutingModule } from './application-routing.module';
 import { ApplicationComponent } from './application.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedComponentsModule } from '../shared-components/shared-components.module';
-import { registration} from'./registration_2/registration_2.component';
 import { registration_2 } from './registration 2/registration 2.component';
 @NgModule({
   declarations: [
