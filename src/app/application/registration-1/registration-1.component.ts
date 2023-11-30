acd431c1054ed0514a8e895ac18ce7c67ca4ef8e
varun <varunx723@gmail.com>
2023-11-30 22:29:39 +0530

    added the text when name is not matched with the validation

--- src/app/application/registration-1/registration-1.component.ts
+++ src/app/application/registration-1/registration-1.component.ts
@@ -3,22 +3,19 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'application-registration-1',
   templateUrl: './registration-1.component.html',
-  styleUrls: ['./registration-1.component.css']
+  styleUrls: ['./registration-1.component.css'],
 })
-export class Registration1Component implements OnInit { 
+export class Registration1Component implements OnInit {
   public userForm!: FormGroup;
   title = 'Angular Reactive Form';
-ngOnInit(): void {
-     this.userForm = new FormGroup({
-       name: new FormControl('', [
-         Validators.required,
-         Validators.minLength(6),
-         Validators.maxLength(16),
-         Validators.pattern(/^[a-zA-Z0-9]+$/),
-       ]),
-     });
+  ngOnInit(): void {
+    this.userForm = new FormGroup({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(16),
+        Validators.pattern(/^[a-zA-Z]+$/),
+      ]),
+    });
+  }
 }
- 
-  
-}
-
