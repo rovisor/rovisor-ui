3d11c7f6b6a266d8ea0b18e977333933a18f3c06
Vedant <vedanttyagi18@gmail.com>
2023-11-30 23:17:09 +0530

    created enum with the country name

--- src/app/application/registration-v2/registration-v2.component.ts
+++ src/app/application/registration-v2/registration-v2.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Enum } from './enum';
+
 
 @Component({
     selector: 'app-registration',
@@ -14,6 +14,5 @@ ngOnInit(): void {
   this.registrationForm = new FormGroup({
     name: new FormControl('',[Validators.required,Validators.minLength(6),Validators.maxLength(16)])
   });
-  console.warn(Enum.India);
 }
 }
