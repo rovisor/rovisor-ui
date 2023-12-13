f63317271bab2c832d44dc3468c65306cd5b72d0
varun <varunx723@gmail.com>
2023-12-13 22:08:08 +0530

    made selection for countries

--- src/app/application/registration-1/registration-1.component.ts
+++ src/app/application/registration-1/registration-1.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgSelectConfig } from '@ng-select/ng-select';
 
+interface CountryInfo {
+  id: number;
+  name: string;
+}
 @Component({
   selector: 'application-registration-1',
   templateUrl: './registration-1.component.html',
@@ -9,6 +14,13 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class Registration1Component implements OnInit {
   public userForm!: FormGroup;
   title = 'Angular Reactive Form';
+  countriesInfo: CountryInfo[] = [
+    { id: 1, name: 'India' },
+    { id: 2, name: 'US' },
+    { id: 3, name: 'UK' },
+    { id: 4, name: 'Australia' },
+    { id: 5, name: 'France' },
+  ];
   ngOnInit(): void {
     this.userForm = new FormGroup({
       name: new FormControl('', [
@@ -22,10 +34,10 @@ export class Registration1Component implements OnInit {
         Validators.required,
         Validators.pattern(/^[1-9]\d{9}$/),
         Validators.maxLength(10),
-        Validators.minLength(10)
+        Validators.minLength(10),
       ]),
       password: new FormControl('', [Validators.required]),
+      country: new FormControl('', [Validators.required]),
     });
   }
 }
-
