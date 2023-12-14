d8b32638a7c84d60bd7eaf8462e0c30301ddada6
Vedant <vedanttyagi18@gmail.com>
2023-12-15 01:15:34 +0530

    conutry enum

--- src/app/application/registration-v2/registration-v2.component.ts
+++ src/app/application/registration-v2/registration-v2.component.ts
@@ -11,14 +11,33 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class RegistrationV2Component implements OnInit {
 public registrationForm!: FormGroup;
 constructor() { }
+minDate = "2022-12-15";
+maxDate = "2180-12-15"
 ngOnInit(): void {
   this.registrationForm = new FormGroup({
-    name: new FormControl('',[Validators.required,Validators.minLength(6),Validators.maxLength(16)]),
-    phone: new FormControl('', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$") ,Validators.maxLength(10),Validators.minLength(10)]),
-    email: new FormControl('',[Validators.required,]),
-    password: new FormControl('',[Validators.required,]),
-    date: new FormControl('',[Validators.required,]),
-    dropdown: new FormControl('',[Validators.required,]),
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(16),
+      Validators.pattern(/^[a-zA-Z]+$/),
+    ]),
+    phone: new FormControl('', [Validators.required, 
+      Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$") ,
+      Validators.maxLength(10),
+      Validators.minLength(10)
+    ]),
+    email: new FormControl('',[
+      Validators.required,
+    ]),
+    password: new FormControl('',[
+      Validators.required,
+    ]),
+    calendar: new FormControl('',[
+      Validators.required,
+    ]),
+    dropdown: new FormControl('',[
+      Validators.required,
+    ]),
   });
 }
 }
