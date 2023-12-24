18b9241331baab08affc86c19a3108c63124beb3
varun <varunx723@gmail.com>
2023-12-24 23:58:53 +0530

    Binded the name field in HTML with user object’s name property

--- src/app/application/registration-1/registration-1.component.ts
+++ src/app/application/registration-1/registration-1.component.ts
@@ -8,6 +8,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class Registration1Component implements OnInit {
   public userForm!: FormGroup;
   title = 'Angular Reactive Form';
+  selectedCountry: any;
+  countries = ARRAY;
   ngOnInit(): void {
     this.userForm = new FormGroup({
       name: new FormControl('', [
@@ -18,4 +20,36 @@ export class Registration1Component implements OnInit {
       ]),
     });
   }
+  submitForm(): void {
+    if (this.userForm.valid) {
+      const val = {
+        name: this.userForm.get('name')?.value,
+        email: this.userForm.get('email')?.value,
+        phoneNumber: this.userForm.get('phoneNumber')?.value,
+        password: this.userForm.get('password')?.value,
+        birthdate: this.userForm.get('birthdate')?.value,
+        country: this.userForm.get('country')?.value,
+      };
+
+      console.log(val);
+    } else {
+      console.log('Form is not valid');
+    }
+  }
+  validatedob(control: FormControl): { [key: string]: boolean } | null {
+    const maxdob = new Date();
+    maxdob.setFullYear(maxdob.getFullYear() - 87);
+
+    if (control.value && new Date(control.value) < maxdob) {
+      return { lessthen87: true };
+    }
+    return null;
+  }
+  validateCountryId(control: FormControl): { [key: string]: boolean } | null {
+    if (!Countries[control.value as keyof typeof Countries]) {
+      return { invalidCountryId: true };
+    }
+    return null;
+  }
 }
+//keyof typeof:-to ensure that control.value is used as a valid key for indexing the enum.
