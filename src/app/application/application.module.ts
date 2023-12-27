f76fc1e485a631401e6545de774974e899f148dc
varun <varunx723@gmail.com>
2023-12-27 23:16:47 +0530

    changed the birthDate

--- src/app/application/application.module.ts
+++ src/app/application/application.module.ts
@@ -4,7 +4,7 @@ import { ApplicationComponent } from './application.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedComponentsModule } from '../shared-components/shared-components.module';
 import { RegistrationV2Component } from './registration-v2/registration-v2.component';
-
+import { CommonModule } from '@angular/common';
 
 
 import { Registration1Component } from './registration-1/registration-1.component';
@@ -26,6 +26,7 @@ import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
     ReactiveFormsModule,
     FormsModule,
     NgSelectModule,
+    CommonModule
   ],
 })
 export class ApplicationModule { }
