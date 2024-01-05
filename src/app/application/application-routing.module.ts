ec6d2e8d2a68bd8f1aa008eb0cdfb95f99ab95bc
varun <varunx723@gmail.com>
2024-01-05 22:08:24 +0530

    removed angular/material and routing path

--- src/app/application/application-routing.module.ts
+++ src/app/application/application-routing.module.ts
@@ -4,7 +4,7 @@ import { ApplicationComponent } from './application.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Registration1Component } from './registration-1/registration-1.component';
 import { RegistrationV2Component } from './registration-v2/registration-v2.component';
-import { UploadStatementComponent } from './upload-statement/upload-statement.component';
+
 
 const routes: Routes = [
   {
@@ -23,10 +23,7 @@ const routes: Routes = [
         path: 'registration-v2',
         component: RegistrationV2Component,
       },
-      {
-        path: 'upload-statement',
-        component: UploadStatementComponent,
-      }
+      
     ],
   }
 ];
