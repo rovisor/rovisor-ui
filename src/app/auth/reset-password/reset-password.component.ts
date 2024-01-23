058d42461da7ce72dc1475822a60967c2afa3371
Vedant <vedanttyagi18@gmail.com>
2024-01-23 18:02:25 +0530

    worked on validation

--- src/app/auth/reset-password/reset-password.component.ts
+++ src/app/auth/reset-password/reset-password.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginResponseModel } from '../state/auth.model';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../state/auth.service';
+
+
 
 @Component({
   selector: 'app-reset-password',
@@ -8,4 +13,24 @@ import { FormGroup } from '@angular/forms';
 })
 export class ResetPasswordComponent {
   public resetPasswordForm!: FormGroup;
-}
+
+  private subscription: Subscription = new Subscription();
+  
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {}
+
+
+  ngOnInit(): void {
+    this.resetPasswordForm = new FormGroup({
+      email: new FormControl('',[Validators.required,  Validators.email])
+    });
+  }
+  onSubmit() {
+    if (this.resetPasswordForm!.invalid) {
+      return;
+    }
+
+    this.subscription.add(this.authService.signup(this.resetPasswordForm!.value).subscribe((response: LoginResponseModel) => {
+      console.log(response);
+    }));
+
+  }}
