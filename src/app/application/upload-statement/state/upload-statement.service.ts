c2fcee76e674ab846d96c20eab7268f339402ce4
varun <varunx723@gmail.com>
2024-02-06 03:44:58 +0530

    Updated upload statement service to use map operator and return response directly.

--- src/app/application/upload-statement/state/upload-statement.service.ts
+++ src/app/application/upload-statement/state/upload-statement.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from "@angular/core"; // allows Angular to inject dependencies into the service.
 import {HttpClient} from '@angular/common/http';
+import { map } from "rxjs/operators";
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' }) 
 export class UploadStatementService {
-    constructor(private httpClient: HttpClient) {
+    constructor(private httpClient: HttpClient) {       
 
     }
+    uploadStatement(formData: FormData) {
+        return this.httpClient.post('http://localhost:5053/api/upload-statement', formData)
+            .pipe(map((response: any) => { return response })); // The map operator takes the response and returns it directly.
+    }
+
 }
