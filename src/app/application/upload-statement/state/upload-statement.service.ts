29ba1b7ef6d99ca78bf5e6e998c6e4fd7aa37206
varun <varunx723@gmail.com>
2024-02-14 01:36:31 +0530

    Add Toastr service to display success and error messages on file upload.

--- src/app/application/upload-statement/state/upload-statement.service.ts
+++ src/app/application/upload-statement/state/upload-statement.service.ts
@@ -1,15 +1,28 @@
-import { Injectable } from "@angular/core"; // allows Angular to inject dependencies into the service.
-import {HttpClient} from '@angular/common/http';
-import { map } from "rxjs/operators";
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { map, catchError } from "rxjs/operators";
+import { throwError } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
-@Injectable({ providedIn: 'root' }) 
+@Injectable({ providedIn: 'root' })
 export class UploadStatementService {
-    constructor(private httpClient: HttpClient) {       
+    constructor(
+        private httpClient: HttpClient,
+        private toastr: ToastrService // Inject ToastrService
+    ) { }
 
-    }
     uploadStatement(formData: FormData) {
-        return this.httpClient.post('http://localhost:5000/api/FileUpload/upload', formData)
-            .pipe(map((response: any) => { return response })); // The map operator takes the response and returns it directly.
-    }
+        return this.httpClient.post('http://localhost:5053/api/FileUpload/upload', formData)
+            .pipe(
+                map((response: any) => { // The map operator takes the response and returns it directly.
+                    this.toastr.success('File uploaded successfully', 'Success');
+                    return response;
+                }),
+                catchError((error: any) => {
 
+                    this.toastr.error('An error occurred while uploading the file.', 'Error');
+                    return throwError(error);
+                })
+            );
+    }
 }
