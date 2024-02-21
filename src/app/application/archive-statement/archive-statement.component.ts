e1daf78ae0a63c0425dbbd7476e2d27878473ed4
Vedant <vedanttyagi18@gmail.com>
2024-02-21 23:58:38 +0530

    added some files

--- src/app/application/archive-statement/archive-statement.component.ts
+++ src/app/application/archive-statement/archive-statement.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 
@@ -13,40 +14,36 @@ export class ArchiveStatementComponent implements OnInit, OnDestroy {
   private subscription: Subscription = new Subscription();
 force: any;
 
+public getJasonValue:any;
+public postJasonValue:any;
 
-  constructor() {}
+  constructor(private http:HttpClient) {}
     
   
 
 
   ngOnInit(): void {
+    this.getMethod();
    
   }
 
   ngOnDestroy(): void {
    
         }
-       rows = [
-          { DateAndTime: '1-feb-2024', Naration: 'Rachit',Amount:'5000', DebitCredit: 'credit',Account:'HDFC'},
-          { DateAndTime: '12-feb-2024', Naration: 'Ramesh',Amount:'60000', DebitCredit: 'Debit',Account:'paytm'},
-          { DateAndTime: '15-feb-2024', Naration: 'Ramkali',Amount:'4400', DebitCredit: 'credit',Account:'g-pay'},
-          { DateAndTime: '13-feb-2024', Naration: 'Rajkumar',Amount:'4300', DebitCredit: 'Debit',Account:'Sbi'},
-          { DateAndTime: '13-feb-2024', Naration: 'Rakesh',Amount:'100', DebitCredit: 'credit',Account:'Sbi'},
-          { DateAndTime: '15-feb-2024', Naration: 'Rajendeer',Amount:'500', DebitCredit: 'Debit',Account:'HDFC'},
-          { DateAndTime: '14-feb-2024', Naration: 'Rachit',Amount:'50', DebitCredit: 'credit',Account:'phonepay'},
-          { DateAndTime: '17-feb-2024', Naration: 'Ram',Amount:'7000', DebitCredit: 'debit',Account:'paytm'},
-          { DateAndTime: '11-feb-2024', Naration: 'Rachit',Amount:'500', DebitCredit: 'credit',Account:'g-pay'},
-          { DateAndTime: '10-feb-2024', Naration: 'Ramishver',Amount:'6000', DebitCredit: 'Debit',Account:'HDFC'},
-                 
-        ];
+        public getMethod(){
+          this.http.get(' http://localhost:5000/api/statement/statement').subscribe((data)=>{
+            console.log(data);
+            this.getJasonValue
+          });
+        }
+        public postMethod(){
+          this.http.post('http://localhost:5000/api/statement/statement',{}).subscribe((data)=>{
+            console.log(data);
+            this.postJasonValue
+          });
+        }
       
-        columns = [
-          { prop: 'DateAndTime', name: 'DateAndTime' },
-          { prop: 'Naration', name: 'Naration' },
-          { prop: 'Amount', name:'Amount'},
-          { prop: 'DebitCredit' ,name:'DebitCredit'},
-          { prop: 'Account',name:'Account'},
-        ];
+       
        }
       
 
