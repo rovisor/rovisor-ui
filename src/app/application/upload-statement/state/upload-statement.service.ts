fce3526404308dcd4853bcf20d05868fa7d24f84
varun <varunx723@gmail.com>
2024-02-21 22:48:15 +0530

    Updated upload statement service to use Toastr for success messages

--- src/app/application/upload-statement/state/upload-statement.service.ts
+++ src/app/application/upload-statement/state/upload-statement.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from "@angular/core";
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { map, catchError } from "rxjs/operators";
+import { throwError } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class UploadStatementService {
-    constructor(private httpClient: HttpClient) {
+    constructor(
+        private httpClient: HttpClient,
+        private toastr: ToastrService
+    ) { }
 
+    uploadStatement(formData: FormData) {
+        const apiUrl = "http://localhost:5053/api/FileUpload/upload"
+        return this.httpClient.post('apiUrl', formData)
+            .pipe(
+                map((response: any) => { // The map operator takes the response and returns it directly.
+                    this.toastr.success('File uploaded successfully', 'Success');
+                    return response;
+                }),
+                
+            );
     }
 }
