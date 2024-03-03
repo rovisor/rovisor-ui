787c6afffe66a01727ca93e9e18dc5e457caa63b
varun <varunx723@gmail.com>
2024-03-04 03:22:37 +0530

    Added account form validation and success toast, update date formats

--- src/app/application/add-account/add-account.component.ts
+++ src/app/application/add-account/add-account.component.ts
@@ -1,22 +1,45 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AddAccountService } from './state/add-account.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-add-account-statement',
   templateUrl: './add-account.component.html',
-  styleUrls: ['./add-account.component.css']
+  styleUrls: ['./add-account.component.css'],
 })
-
 export class AddAccountComponent implements OnInit, OnDestroy {
   public accountForm!: FormGroup;
   private subscription: Subscription = new Subscription();
-  constructor(private formBuilder: FormBuilder, private addAccountService: AddAccountService) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private addAccountService: AddAccountService,
+    private toastr: ToastrService
+  ) {}
   ngOnInit() {
+    this.accountForm = this.formBuilder.group({
+      accountName: [
+        '',
+        [Validators.required, Validators.pattern('^[a-zA-Z0-9 ]+$')],
+      ],
+      institutionName: [
+        '',
+        [Validators.required, Validators.pattern('^[a-zA-Z]+$')],
+      ],
+      selectedAccount: ['', Validators.required],
+      accountNumber: [
+        '',
+        [Validators.required, Validators.pattern('^[0-9]{4}$')],
+      ],
+    });
   }
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+  onSubmit() {
+    if (this.accountForm.valid) {
+      this.toastr.success('Account added successfully', 'Success!');
+    }
   }
 }
