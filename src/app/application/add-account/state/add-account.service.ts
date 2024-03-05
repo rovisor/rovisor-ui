866b0fec3628ebece3a3a06bad47ad10cb3639b9
varun <varunx723@gmail.com>
2024-03-06 03:41:56 +0530

    Fix add account API URL

--- src/app/application/add-account/state/add-account.service.ts
+++ src/app/application/add-account/state/add-account.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from "@angular/core";
 import {HttpClient} from '@angular/common/http';
-import { map } from "rxjs";
-import { environment } from "src/environments/environment"; 
+import { map,catchError } from "rxjs";
+import { throwError } from 'rxjs';
+import { environment } from "src/environments/environment";
 
 @Injectable({providedIn: 'root'})
 export class AddAccountService {
-    constructor(private httpClient: HttpClient) {}
+    constructor(private httpClient: HttpClient) { }
+    
+    addAccount(accountData: FormData) {
+        return this.httpClient.post(`${environment.apiUrl}/api/useraccount`, accountData).pipe(catchError((error: any) => { return throwError(error); }));
+    }
 }
