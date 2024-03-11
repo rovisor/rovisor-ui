849cd2162a5ea48c83c33c8ab39d59ed4714b616
varun <varunx723@gmail.com>
2024-03-12 02:38:19 +0530

    Updated UI and validation for add account form

--- src/app/application/add-account/add-account.component.ts
+++ src/app/application/add-account/add-account.component.ts
@@ -1,22 +1,32 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { AddAccountService } from './state/add-account.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import { AddAccountService } from './state/add-account.service';
 
 @Component({
-  selector: 'app-add-account-statement',
+  selector: 'add-account.statement',
   templateUrl: './add-account.component.html',
   styleUrls: ['./add-account.component.css'],
 })
 export class AddAccountComponent implements OnInit, OnDestroy {
+  accountTypes: string[] = [
+    "Savings Account",
+    "Wallet",
+    "Current Account",
+    "Loan Account",
+    "Investment Account"
+  ];
+
   public accountForm!: FormGroup;
   private subscription: Subscription = new Subscription();
+
   constructor(
     private formBuilder: FormBuilder,
+    private toastr: ToastrService,
     private addAccountService: AddAccountService,
-    private toastr: ToastrService
-  ) {}
+  ) { }
+
   ngOnInit() {
     this.accountForm = this.formBuilder.group({
       accountName: [
@@ -27,19 +37,35 @@ export class AddAccountComponent implements OnInit, OnDestroy {
         '',
         [Validators.required, Validators.pattern('^[a-zA-Z ]+$')],
       ],
-      selectedAccount: ['', Validators.required],
+      accountSelect: ['', Validators.required],
       accountNumber: [
         '',
         [Validators.required, Validators.pattern('^[0-9]{4}$')],
       ],
     });
+
+    this.accountForm.patchValue({
+      accountSelect: this.accountTypes[0]
+    });
+    this.accountForm.valueChanges.subscribe(() => { // This line sets up a subscription to the valueChanges observable of the accountForm
+      for (const cValue in this.accountForm.controls) {
+        if (this.accountForm.controls[cValue].invalid) {
+          this.accountForm.controls[cValue].markAsTouched();
+        }
+      }
+    });
   }
+
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
+
   onSubmit() {
     if (this.accountForm.valid) {
       this.toastr.success('Account added successfully', 'Success!');
+    }
+    else {
+      this.toastr.error('Please fill all required fields correctly', 'Error');
     }
   }
 }
