ef12ed9c543fe1f99c2248d334fa2d6fc2ec927e
Vedant <vedanttyagi18@gmail.com>
2024-04-11 00:23:28 +0530

    worked on form control

--- src/app/application/dashboard/dashboard.component.ts
+++ src/app/application/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { UploadStatementComponent } from '../upload-statement/upload-statement.component';
 import { AddAccountComponent } from '../add-account/add-account.component';
+import { FormGroup,FormBuilder } from '@angular/forms';
+import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,10 +11,46 @@ import { AddAccountComponent } from '../add-account/add-account.component';
   styleUrls: ['./dashboard.component.css']
 })
 
-export class DashboardComponent {
-  constructor(private modalService: NgbModal) { }
+export class DashboardComponent implements OnInit {
+openAddAccountModal() {
+  this.modalService.open(AddAccountComponent, { centered: true, size: 'md', });
+}
+  public statementFiltersForm!: FormGroup;
+  constructor(private modalService: NgbModal,private formBuilder: FormBuilder,) { }
+  ngOnInit(): void {
+    this.statementFiltersForm = this.formBuilder.group({
+      fromDate: [null],
+      toDate: [null],
+    this:fetchStatements(),
+      })
 
-  openAddAccountModal() {
-    this.modalService.open(AddAccountComponent, { centered: true, size: 'md', });
+  fetchStatements();{
+    let fromDate =  null;
+    if(this.statementFiltersForm.value.fromDate) {
+      fromDate = DateTime.fromObject({
+        year: this.statementFiltersForm.value.fromDate?.year,
+        month: this.statementFiltersForm.value.fromDate?.month,
+        day: this.statementFiltersForm.value.fromDate?.day,
+      }).toFormat('yyyy-MM-dd');
+    } 
+
+    let toDate =  null;
+    if(this.statementFiltersForm.value.toDate) {
+      toDate = DateTime.fromObject({
+        year: this.statementFiltersForm.value.toDate?.year,
+        month: this.statementFiltersForm.value.toDate?.month,
+        day: this.statementFiltersForm.value.toDate?.day,
+      }).toFormat('yyyy-MM-dd');
+    } 
+
+    let params = {
+      FromDate:  fromDate,
+      ToDate: toDate,
+}
+}
   }
 }
+function fetchStatements() {
+  throw new Error('Function not implemented.');
+}
+
