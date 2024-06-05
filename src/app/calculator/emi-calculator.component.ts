e0dfb4a8e1e1e85ead0884ce5ce044b366b41413
varun <varunx723@gmail.com>
2024-06-06 05:19:49 +0530

    Added new EMI calculator component with HTML and CSS files

---
+++ src/app/calculator/emi-calculator.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-emi-calculator',
+    templateUrl: './emi-calculator.component.html',
+    styleUrls: ['./emi-calculator.component.css']
+})
+export class NotFoundComponent {
+}
