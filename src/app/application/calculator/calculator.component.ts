d3dae0d52a13275d12e3d3886fc9106e05856c9f
Vedant <vedanttyagi18@gmail.com>
2024-06-26 13:54:48 +0530

    added the output

--- src/app/application/calculator/calculator.component.ts
+++ src/app/application/calculator/calculator.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 @Component({
-    selector: 'app-claculator',
-    templateUrl: './calculator.component.html',
-    styleUrls: ['./calculator.component.css']
-  })
+  selector: 'app-claculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
 
-  export class CalculatorComponent implements OnInit, OnDestroy {
-    ngOnDestroy(): void {
-        throw new Error('Method not implemented.');
-    }
-    ngOnInit(): void {
-        throw new Error('Method not implemented.');
-    }
+export class CalculatorComponent {
 
+  CurrentAge: number | null = null;
+  RetirementAge: number | null = null;
+  MonthlyContribution: number | null = null;
+  ProjectedRetirementSavings: number = 0;
+  TotalContributions: number = 0;
+  TotalInterestEarned: number = 0;
+  calculateEMI() {
   }
+  resetForm() {
+    this.CurrentAge = null;
+    this.RetirementAge = null;
+    this.MonthlyContribution = null;
+  }
+
+
+}
