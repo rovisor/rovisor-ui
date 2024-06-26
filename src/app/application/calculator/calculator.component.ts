60e1e72fb7207adee1f1db8824d7df8913f3b43b
Vedant <vedanttyagi18@gmail.com>
2024-06-26 19:19:25 +0530

    adjusted the output

--- src/app/application/calculator/calculator.component.ts
+++ src/app/application/calculator/calculator.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-claculator',
@@ -8,20 +9,31 @@ import { Subscription } from 'rxjs';
 })
 
 export class CalculatorComponent {
+  retirementForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.retirementForm = this.fb.group({
+      currentAge: [null, Validators.required],
+      annualRate: [null, [Validators.required, Validators.min(0)]],
+      retirementAge: [null, Validators.required],
+      monthlyContribution: [null, Validators.required]
+    });
+  }
   CurrentAge: number | null = null;
   RetirementAge: number | null = null;
   MonthlyContribution: number | null = null;
+  AnnualRate: number | null = null;
+  Saving: number | null = null;
   ProjectedRetirementSavings: number = 0;
   TotalContributions: number = 0;
   TotalInterestEarned: number = 0;
   calculateEMI() {
   }
-  resetForm() {
+resetForm() {
     this.CurrentAge = null;
     this.RetirementAge = null;
     this.MonthlyContribution = null;
-  }
+}
 
 
 }
