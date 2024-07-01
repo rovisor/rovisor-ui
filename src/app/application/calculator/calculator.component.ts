7d5caf75476f884d86a7a606a85f63c9e2996e12
Vedant <vedanttyagi18@gmail.com>
2024-07-02 00:36:22 +0530

    Added the functinallity and UI design for the Mortage Calculator

--- src/app/application/calculator/calculator.component.ts
+++ src/app/application/calculator/calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent {
+  
+MortageCalculatorForm: FormGroup;
 
+  ProjectedRetirementSavings: number = 0;
+  errorMessage: string = '';
+
+  constructor(private fb: FormBuilder) {
+    this.MortageCalculatorForm = this.fb.group({
+      loanAmount: [null, Validators.required],
+      annualRate: [null, [Validators.required,]],
+      time : [null, Validators.required],
+    });
+  }
+
+  calculate() {
+    if (this.MortageCalculatorForm.valid) {
+      const loanAmount = this.MortageCalculatorForm.value.loanAmount;
+      const annualRate = this.MortageCalculatorForm.value.annualRate ;
+      const time = this.MortageCalculatorForm.value.time;
+
+      if (loanAmount <= 0 || annualRate <= 0 || time <= 0) {
+        this.errorMessage = " All values must be greater than zero.";
+        return; 
+      }
+
+
+      const lonaTerm = (time*12) ;
+      const ratePerMonth = annualRate / 12;
+
+      this.ProjectedRetirementSavings = (loanAmount*((ratePerMonth*(Math.pow(1 +(ratePerMonth),(lonaTerm))))/(((Math.pow(1 +(ratePerMonth),(lonaTerm)))-1))));
+
+      this.errorMessage = '';
+    } else {
+      this.errorMessage = "Please fill out all required fields.";
+    }
+  }
+  
+
+  resetForm() {
+    this.MortageCalculatorForm.reset();
+    this.ProjectedRetirementSavings = 0;
+    this.errorMessage = '';
+  }
 }
