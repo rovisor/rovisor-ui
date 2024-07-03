3c69b7316b4601d7b34ecb062df52276fcff0af9
Vedant <vedanttyagi18@gmail.com>
2024-07-03 22:14:34 +0530

    Applied the formula and and the UI design

--- src/app/application/InvestmentCalculator/InvestmentCalculator.component.ts
+++ src/app/application/InvestmentCalculator/InvestmentCalculator.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./InvestmentCalculator.component.css']
 })
 export class InvestmentCalculatorComponent {
+    InvestmentCalculatorForm: FormGroup;
 
+    Projectedcalculation: number = 0;
+    errorMessage: string = '';
+  
+    constructor(private fb: FormBuilder) {
+      this.InvestmentCalculatorForm = this.fb.group({
+        InitialInvestment: [null, Validators.required],
+        FinalValue: [null, Validators.required],
+      });
+    }
+  
+    calculate() {
+      if (this.InvestmentCalculatorForm.valid) {
+        const InitialInvestment = this.InvestmentCalculatorForm.value.InitialInvestment;
+        const FinalValue = this.InvestmentCalculatorForm.value.FinalValue;
+  
+        if (InitialInvestment <= 0 || FinalValue<= 0 ) {
+          this.errorMessage = " All values must be greater than zero.";
+          return; 
+        }
+  
+
+        
+        this.Projectedcalculation = (((FinalValue-InitialInvestment)/InitialInvestment)*100);
+  
+        this.errorMessage = '';
+      } else {
+        this.errorMessage = "Please fill out all required fields.";
+      }
+    }
+    
+  
+    resetForm() {
+      this.InvestmentCalculatorForm.reset();
+      this.Projectedcalculation = 0;
+      this.errorMessage = '';
+    }
 }
