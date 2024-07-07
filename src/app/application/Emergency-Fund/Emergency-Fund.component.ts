536bfa8af83bc4359e72886fb5c5ad22950b185e
Vedant <vedanttyagi18@gmail.com>
2024-07-07 23:20:29 +0530

    Made the componenet

---
+++ src/app/application/Emergency-Fund/Emergency-Fund.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './Emergency-Fund.component.html',
+  styleUrls: ['./Emergency-Fund.component.css']
+})
+export class EmergencyFundComponent {
+
+}
