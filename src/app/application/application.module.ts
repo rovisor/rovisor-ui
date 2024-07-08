e9261e24d74207d0be0153264f23b652190c18c6
varun <varunx723@gmail.com>
2024-07-09 04:58:26 +0530

    Added Debt-to-Income Ratio Calculator component and route to application module and routing module, and updated sidebar.json with new menu item

--- src/app/application/application.module.ts
+++ src/app/application/application.module.ts
@@ -17,7 +17,7 @@ import { AccountMappingComponent } from './account-mapping/account-mapping.compo
 import { AddAccountComponent } from './add-account/add-account.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AccountDetailsComponent } from './account-details/account-details.component';
-
+import { DebttoIncomeRatioCalculatorComponent } from './diratio-calculator/diratio-calculator.component';
 @NgModule({
   declarations: [
     ApplicationComponent,
@@ -28,7 +28,8 @@ import { AccountDetailsComponent } from './account-details/account-details.compo
     AccountMappingComponent,
     AddAccountComponent,
     DashboardComponent,
-    AccountDetailsComponent
+    AccountDetailsComponent,
+    DebttoIncomeRatioCalculatorComponent
   ],
   imports: [
     NgbDatepickerModule,
---
+++ src/app/application/diratio-calculator/diratio-calculator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+    selector: 'app-calculator',
+    templateUrl: './diratio-calculator.component.html',
+    styleUrls: ['./diratio-calculator.component.css']
+})
+export class DebttoIncomeRatioCalculatorComponent {
+   
+}
