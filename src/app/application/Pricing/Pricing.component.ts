4e97e6b97cd6607b5b3ad1b4563a31d89d696995
Vedant <vedanttyagi18@gmail.com>
2024-07-29 23:38:48 +0530

    added the funtionality

--- src/app/application/Pricing/Pricing.component.ts
+++ src/app/application/Pricing/Pricing.component.ts
@@ -7,6 +7,50 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./Pricing.component.css']
 })
 export class PricingComponent {
+  pricingForm: FormGroup;
 
+  cost: number = 0;
+  price: number = 0;
+  errorMessage: string = '';
+
+  constructor(private fb: FormBuilder) {
+    this.pricingForm = this.fb.group({
+      productionCost: [null, Validators.required],
+      fulfillmentCost: [null, Validators.required],
+      customerAcquisitionCost: [null, Validators.required],
+      averageSubscriptionMonths: [null, Validators.required]
+    });
+  }
+
+  calculate() {
+    if (this.pricingForm.valid) {
+      const productionCost = this.pricingForm.value.productionCost;
+      const fulfillmentCost = this.pricingForm.value.fulfillmentCost;
+      const customerAcquisitionCost = this.pricingForm.value.customerAcquisitionCost;
+      const averageSubscriptionMonths = this.pricingForm.value.averageSubscriptionMonths;
+
+      if (productionCost <= 0 || fulfillmentCost <= 0 || customerAcquisitionCost <= 0 || averageSubscriptionMonths <= 0) {
+        this.errorMessage = "All values must be greater than zero.";
+        return; 
+      }
+
+    
+
+      this.cost = (productionCost+ fulfillmentCost+(customerAcquisitionCost/averageSubscriptionMonths));
+      
+      this.price = (this.cost*1.2);
+
+
+      this.errorMessage = '';
+    } else {
+      this.errorMessage = "Please fill out all required fields.";
+    }
+  }
+
+  resetForm() {
+    this.pricingForm.reset();
+    this.cost = 0;
+    this.price = 0;
+    this.errorMessage = '';
+  }
 }
-  
