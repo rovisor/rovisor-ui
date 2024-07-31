02c61c1b1bce94ebfe6d89f7321eba1c84d9efa1
Vedant <vedanttyagi18@gmail.com>
2024-07-31 22:37:14 +0530

    added the retrun

--- src/app/application/Pricing/Pricing.component.ts
+++ src/app/application/Pricing/Pricing.component.ts
@@ -37,6 +37,7 @@ export class PricingComponent {
       }
       if (fulfillmentCost < 0 || customerAcquisitionCost <0 || ProfitMargin <0){
         this.errorMessage = "All values must be  non-negative";
+        return;
       }
       if (ProfitMargin >= 1) {
         this.errorMessage = "Profit margin must be less than 100%.";
